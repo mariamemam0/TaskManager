c0a239db3a418c60e74a3827a62dcecd40228d7d
marim <mariamemampp00@gmail.com>
2026-05-23 16:46:58 +0300

    handle the data of the project table

--- database/migrations/2026_05_23_131931_create_projects_table.php
+++ database/migrations/2026_05_23_131931_create_projects_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
