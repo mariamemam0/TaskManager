d43b54d4c4374d3e36198b92fbd8e0c1162eb560
marim <mariamemampp00@gmail.com>
2026-05-24 16:16:50 +0300

    handle resources and collections for project and task data

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+          return [
+              'id'=> $this->id,
+              'project'=>new ProjectResource($this->project),
+              'title'=> $this->title,
+              'slug'=>$this->slug,
+              'status'=> $this->status,
+              'description'=> $this->description,
+              'started_at'=>$this->started_at,
+              'ended_at'=> $this->ended_at
+          ];
+    }
+}
