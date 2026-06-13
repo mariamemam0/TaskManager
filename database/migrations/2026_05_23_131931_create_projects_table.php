3c85efd2b840e19f820bc0bcfd7a21f33c01e83c
marim <mariamemampp00@gmail.com>
2026-06-13 11:59:40 +0300

    edit the relation between user and project => many to many

--- database/migrations/2026_05_23_131931_create_projects_table.php
+++ database/migrations/2026_05_23_131931_create_projects_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
