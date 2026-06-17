4f1e764be078eb3361a55ca5786d081467182ef1
marim <mariamemampp00@gmail.com>
2026-06-17 20:47:58 +0300

    Attach the middleware to login route

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::prefix('{type}/{id}/comments')->group(function () {
 
 
 Route::post('register',[\App\Http\Controllers\AuthController::class,'register']);
-Route::post('login',[\App\Http\Controllers\AuthController::class,'login']);
+Route::post('login',[\App\Http\Controllers\AuthController::class,'login'])->middleware('throttle:login');
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout',   [\App\Http\Controllers\AuthController::class, 'logout']);
