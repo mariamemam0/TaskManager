bb20b9eeb9b0a0242bca2f04b9c9597f0f46c8c6
marim <mariamemampp00@gmail.com>
2026-06-24 19:14:17 +0300

    add permission and roles from spatie package

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,13 +12,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable ,HasApiTokens;
+    use HasFactory, Notifiable ,HasApiTokens , HasRoles;
 
     /**
      * Get the attributes that should be cast.
